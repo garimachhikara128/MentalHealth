d56bf9e6e5ba26f087d368272c99d63619e99eda
Garima Chhikara <garima@Garimas-MacBook-Pro.local>
2024-03-29 22:38:06 +0530

    First Commit

---
+++ BERTTesting.py
@@ -0,0 +1,100 @@
+import numpy as np
+import pandas as pd
+import time
+import datetime
+import gc
+import random
+import re
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler,random_split
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import transformers
+from transformers import BertForSequenceClassification, AdamW, BertConfig,BertTokenizer,get_linear_schedule_with_warmup
+import json
+from sklearn.metrics import classification_report
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+model = BertForSequenceClassification.from_pretrained(
+    "bert-base-uncased", # Use the 12-layer BERT model, with an uncased vocab.
+    num_labels = 2, # The number of output labels--2 for binary classification.
+                    # You can increase this for multi-class tasks.   
+    output_attentions = False, # Whether the model returns attentions weights.
+    output_hidden_states = False, # Whether the model returns all hidden-states.
+)
+model = model.to(device)
+model.load_state_dict(torch.load('bert_model', map_location=torch.device(device)))
+model.eval()
+
+batch_size = 16
+
+test_input_ids = []
+test_attention_masks = []
+
+# Load the BERT tokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+
+gold = []
+file_name = ['amlonely', 'beensuicidal', 'cutmyself', 'endmylife', 'hangmyself', 'hatemyself', 'havenoone', 'killmyself', 'selfharm', 'selfinjury', 'suicidalthoughts', 'takemylife', 'wantdeath', 'wanttocommitsuicide', 'wanttodie'] 
+
+for each_file in file_name :
+
+    f = open('./Tweets/label_' + each_file + '.txt', 'rt')
+    
+    test_tweets = f.read().split("\n$$$$$$$$$$\n")
+    for each_tweet in test_tweets[0:-1]:
+
+        json_each_tweet = json.loads(each_tweet)
+        text = re.sub(r"[^a-zA-Z?.!,¿]+", " ", json_each_tweet['text']) # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
+        text = re.sub(r"http\S+", "",text) #Removing URLs
+        text = re.sub(r"https\S+", "",text) #Removing URLs 
+        encoded_dict = tokenizer.encode_plus(
+                                # json_each_tweet['text'],  
+                                text,                   
+                                add_special_tokens = True, 
+                                max_length = 512,           
+                                pad_to_max_length = True,
+                                return_attention_mask = True,
+                                return_tensors = 'pt',
+                        )
+        test_input_ids.append(encoded_dict['input_ids'])
+        test_attention_masks.append(encoded_dict['attention_mask'])
+        # print(json_each_tweet)
+        print(text)
+        gold.append(int(json_each_tweet['label']))
+
+test_input_ids = torch.cat(test_input_ids, dim=0)
+test_attention_masks = torch.cat(test_attention_masks, dim=0)
+
+test_dataset = TensorDataset(test_input_ids, test_attention_masks)
+test_dataloader = DataLoader(
+            test_dataset, # The validation samples.
+            sampler = SequentialSampler(test_dataset), # Pull out batches sequentially.
+            batch_size = batch_size # Evaluate with this batch size.
+        )
+
+predictions = []
+for batch in test_dataloader:
+    
+    b_input_ids = batch[0].to(device)
+    b_input_mask = batch[1].to(device)
+    with torch.no_grad():        
+        output= model(b_input_ids, 
+                                   token_type_ids=None, 
+                                   attention_mask=b_input_mask)
+        logits = output.logits
+        logits = logits.detach().cpu().numpy()
+        pred_flat = np.argmax(logits, axis=1).flatten()
+            
+        predictions.extend(list(pred_flat))
+
+print(predictions)
+print("\n\n\n")
+print(gold)
+
+print(len(gold), len(predictions))
+
+print(classification_report(gold, predictions, target_names=['0','1']))
